e9a347d274b1e97389123f9745498b4267de9924
Rahul Menon <rahul.menon85280@gmail.com>
2023-11-14 15:24:46 -0500

    all

--- function_pointers.c
+++ function_pointers.c
@@ -4,7 +4,12 @@ int add(int x, int y) {
     return x + y;
 }
 
+int func(int x, int y, int(*f)(int, int)) {
+    return f(x, y);
+} 
+
 int main() {
-    int (*fptr) (int, int) = &add;
-    printf("%d", fptr(3, 2));
+    int (*fptr) (int, int) = add;
+    printf("%d\n", fptr(3, 2));
+    printf("%d", func(3, 2, add));
 }
